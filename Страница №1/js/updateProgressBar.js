ef5dcdf6c205bce43831d09a3e53023e4dca15b9
Bator Zhambalov <bat.zham@gmail.com>
2023-06-30 14:37:14 +0800

    first commit

---
+++ Страница №1/js/updateProgressBar.js
@@ -0,0 +1,15 @@
+function updateProgressBar(progressBar) {
+  var value = parseInt(progressBar.dataset.value);
+  var maxValue = parseInt(progressBar.dataset.maxValue);
+  var progress = (maxValue - value) / maxValue * 100;
+  var progressElement = progressBar.getElementsByClassName("progress")[0];
+  progressElement.style.width = progress + "%"; 
+}
+
+
+var progressBars = document.getElementsByClassName("progress-bar");
+
+for (var i = 0; i < progressBars.length; i++) {
+  var progressBar = progressBars[i];
+  updateProgressBar(progressBar);
+}
